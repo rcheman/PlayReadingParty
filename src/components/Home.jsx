7583a4411ae746b1a8336cdc7304753b891ead18
Matthew Cheman <mattcheman@pm.me>
2023-02-27 13:36:11 -0500

    Adjust new actor endpoint to work with new database structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,45 +2,36 @@ import React, { useState, useRef, useEffect } from 'react';
 import CharacterList from './characterList';
 
 const Home = ({ setActors, actors, currentScript }) => {
-  // const [actors, setActors] = useState([]);
-
-  const actorFirstName = useRef('');
-  const actorLastName = useRef('');
+  const newActorInput = useRef('');
 
   // submit of actor name handler
-  const handleInput = (event) => {
-    let newActors = [...actors];
-    let actor = {
-      firstName: actorFirstName.current.value,
-      lastName: actorLastName.current.value,
-    };
-    newActors.push(actor);
-    // set actors in state to be our new actors
-    setActors(newActors);
-    // reset input value to empty string to clear out old input
-    actorFirstName.current.value = '';
-    actorLastName.current.value = '';
+  const newActorHandler = (event) => {
+    event.preventDefault();
+    const newActor = {name: newActorInput.current.value};
 
-    // fetch request to the backend to add the new actor to the database
+    // Add the new actor to the database
     fetch('/actors', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(actor),
+      body: JSON.stringify(newActor),
+    }).then(() => {
+      // set actors in state to be our new actors
+      setActors([...actors, newActor]);
+      // reset input value to empty string to clear out old input
+      newActorInput.current.value = '';
     }).catch((error) => {
       console.error('Error: ', error);
     });
-    event.preventDefault();
   };
 
   // get the actor names and display current actor names
   return (
     <div id='home'>
-      <form onSubmit={handleInput}>
-        <input ref={actorFirstName} placeholder="enter actor's first name" />
-        <input ref={actorLastName} placeholder="enter actor's last name" />
-        <button type='submit'>Add actors to list</button>
+      <form onSubmit={newActorHandler}>
+        <input ref={newActorInput} placeholder="enter actor's name" />
+        <button type='submit'>Add actor</button>
       </form>
       <ul id='actorList'>
         <h3>Actors</h3>

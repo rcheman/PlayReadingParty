d20a6b9bdf97bb3936bdfcff9195c0b1463d1b6a
Rachel Cheman <rachelcheman@gmail.com>
2023-03-10 11:57:03 -0500

    Updates playData to parse all of the scripts in scriptUploads when it first boots up and changes import values in controllers.

--- server/playData.js
+++ server/playData.js
@@ -1,13 +1,26 @@
 const fs = require('node:fs');
-const path = require('path');
+require('dotenv').config();
 
-const twelfthNightPath = '../playScript/twelfth_night.txt';
-const testScriptPath = '../playScript/testPlay.txt';
+const playData = processScripts(process.env.UPLOADPATH);
 
-const playData = {};
+function processScripts(uploadPath) {
+  const scripts = {};
+  const files = fs.readdirSync(uploadPath);
+  files.forEach((file) => {
+    const path = uploadPath + file;
+    const temp = parseScript(path);
+    scripts[temp.title] = temp;
+  });
 
-function parseScript(playPath, title) {
-  const script = fs.readFileSync(path.resolve(__dirname, playPath), 'utf8');
+  return scripts;
+}
+
+// TODO: Add handling for other script formats ex: CHARACTERNAME. these are lines of text.
+//  currently only support CHARACTERNAME. \n these are lines of text
+//  similarly for splitting line chunks
+
+function parseScript(playPath) {
+  const script = fs.readFileSync(playPath, 'utf8');
   // Split script on double \n to get individual
   const splitLines = script.split('\n\n');
   const fullPlay = [];
@@ -19,10 +32,19 @@ function parseScript(playPath, title) {
     const characterLines = splitLines[i].split('\n');
     fullPlay.push(characterLines);
 
-    // get the character name and remove dot from the end
-    let name = characterLines[0];
-    name = name.replace('.', '');
+    let firstLine = characterLines[0];
+    // get the script title from the text file
+    // only check for a title if there hasn't been one assigned
+    if (!scriptData.title) {
+      const words = firstLine.split(' ');
+      if (words[0] === 'Title:') {
+        scriptData.title = words.slice(1).join(' ');
+      }
+    }
 
+    // get the character name and remove dot from the end
+    let name = firstLine;
+    name = name.replace('.', '');
     // check that the name is actually a name and not a different part of the play ex: ACT I
     if (isName(name)) {
       // count lines for the current character chunk
@@ -41,30 +63,25 @@ function parseScript(playPath, title) {
       }
     }
   }
-
-  function isUppercase(str) {
-    return str === str.toUpperCase();
-  }
-
-  // checks if a name is a valid name
-  function isName(name) {
-    return isUppercase(name) && name.length > 1 && name.length < 15 && !name.match(/(ACT)/);
-  }
-
-  // character object constructor
-  function Character(name, lineCount, speaksNum) {
-    this.name = name;
-    this.lineCount = lineCount;
-    this.speaksNum = speaksNum;
-  }
-  scriptData.title = title;
+  // save the full play and character objects to return out
   scriptData.fullPlay = fullPlay;
   scriptData.characterObjs = characterObjs;
 
-  playData[scriptData.title] = scriptData;
+  return scriptData;
 }
 
-const TestPlay = parseScript(testScriptPath, 'Test Play');
-const TwelfthNight = parseScript(twelfthNightPath, 'Twelfth Night');
+function isUppercase(str) {
+  return str === str.toUpperCase();
+}
+// checks if a name is a valid name
+function isName(name) {
+  return isUppercase(name) && name.length > 1 && name.length < 20 && !name.match(/(ACT)/);
+}
+// character object constructor
+function Character(name, lineCount, speaksNum) {
+  this.name = name;
+  this.lineCount = lineCount;
+  this.speaksNum = speaksNum;
+}
 
-module.exports =  playData;
+module.exports = { playData, parseScript };

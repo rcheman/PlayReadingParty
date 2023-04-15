c4cec43da5db309c8fd1c3355ffda0c2c916d5b0
Rachel Cheman <rachelcheman@gmail.com>
2023-04-15 14:44:19 -0400

    Minor update to podman setup script and add note. Move index.js into src instead of public since the router is contained in it.

--- dev.sh
+++ dev.sh
@@ -10,9 +10,11 @@ if [ ! "$(podman ps -a -q -f name=postgres-db)" ]; then
     podman run -v play_volume:/var/lib/postgresql/data \
     --name postgres-db -e POSTGRES_PASSWORD=pass \
     -e POSTGRES_USER=playreading -e POSTGRES_DB=playreadingparty \
-    -d play-reading 
+    -p 5432:5432 -d play-reading
 fi
 
 # start development server
 NODE_ENV=development \
 concurrently "webpack-dev-server --color" "nodemon --inspect ./src/server.js"
+
+# TODO: add something that watches for when the server is exited, then kill and remove the container

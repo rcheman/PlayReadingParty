94b861580b43664aaeb225a723eb35c2e838d38a
Matthew Cheman <mattcheman@pm.me>
2023-05-24 13:11:41 -0400

    Change readingDots reporting rate from 50ms to 5ms. Report every 5ms while scrolling instead of just when scrolling finishes.

--- src/components/ReadingDots.jsx
+++ src/components/ReadingDots.jsx
@@ -38,23 +38,47 @@ const ReadingDots = ({ actors, currentActor, currentScriptId }) => {
     source.addEventListener('message', positionReceiver);
 
     let scrollTimeout;
+    let lastReportedTime = performance.now();
 
     let positionReporter = () => {
       // replace a currently queued update if we're still scrolling before the update was sent.
       // this helps reduce many small changes into one larger change request
       clearTimeout(scrollTimeout);
 
-      scrollTimeout = setTimeout(() => {
+      function report() {
+        lastReportedTime = performance.now();
+
+        // Find the distance between the top of the page and the beginning of the script section
         let readingDotsHeightOffset =
           document.getElementsByTagName('body')[0].getBoundingClientRect().height -
           document.getElementById('readingDots').getBoundingClientRect().height;
 
+        // Position is the percentage (between 0.0 and 1.0) that the user has scrolled through the entire script. 0.0 is
+        // the beginning and 1.0 is the very last line. We subtract readingDotsHeightOffset so the calculations only
+        // involve the script section.
+        // |--------------|  <--------------------------------| <-----|
+        // |    Title     |                                   |body   |readingDotsHeightOffset
+        // |              |                                   |       |
+        // | |Actor 1.    |  <---------------|                | <-----|
+        // | |My line     |                  |script section  |
+        // |           <==|  <--reading dot  |                |
+        // | |Actor 2.    |                  |                |
+        // | |Their line  |                  |                |
+        // |______________|  <---------------| <---------------
         let position = (window.scrollY - readingDotsHeightOffset) / (document.body.scrollHeight - readingDotsHeightOffset);
         position = position.toPrecision(4);
 
         // fetch request to update position
         postPosition(currentActor.id, currentScriptId, position);
-      }, 50);
+      }
+
+      // Update while scrolling if it has been going on for more than 5ms, otherwise delay until 5ms have elapsed.
+      // This ensures that other users see scrolling changes as they occur rather than only after they've stopped.
+      if (performance.now() - lastReportedTime > 5) {
+        report();
+      } else {
+        scrollTimeout = setTimeout(report, 5);
+      }
     };
 
     addEventListener('scroll', positionReporter);

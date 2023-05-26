ba188d85218462f9beb44ac873b39b2b011687e6
Rachel Cheman <rachelcheman@gmail.com>
2023-05-26 12:15:49 -0400

    Adds documentation to the character and dot repositories.

--- src/repository/characterRepository.js
+++ src/repository/characterRepository.js
@@ -1,6 +1,13 @@
 const db = require('./database');
 const { Character } = require('../services/scriptParser');
 
+/**
+ * Get all the characters for the current script.
+ * Also can get the characters for a specified actor
+ * @param {string} scriptId ID of the selected script
+ * @param {string | number} actorId optional: ID of the selected actor. Default value of -1 to allow SQL query even when no ID is given
+ * @return {Promise<{Character}>} Character objects indexed by their id
+ */
 async function getCharacters(scriptId, actorId = -1) {
   const result = await db.query(
     `
@@ -10,21 +17,28 @@ async function getCharacters(scriptId, actorId = -1) {
         AND script_id = $2`,
     [actorId, scriptId]
   );
-
   const characters = {};
   result.rows.forEach((c) => characters[c.id.toString()] = new Character(c.name, c.line_count, c.speaks_count, c.id.toString(), c.actor_id));
   return characters;
 
-};
+}
 
+/**
+ * Assign a character to a specified actor.
+ * @param {string} scriptId ID of the selected script
+ * @param {string} characterId ID of the selected character that will be assigned
+ * @param {string} actorId ID of the actor that the character will be assigned to
+ * @return {Promise<boolean>} Whether on to the query went through
+ */
 async function assignCharacter(scriptId, characterId, actorId) {
-  return await db.query(`
+  const result = await db.query(`
     UPDATE characters
     SET actor_id = $1
     WHERE script_id = $2 AND id = $3
     RETURNING *`,
     [actorId, scriptId, characterId]
   );
-};
+  return !!result.rows;
+}
 
 module.exports = { getCharacters, assignCharacter };
--- src/repository/dotRepository.js
+++ src/repository/dotRepository.js
@@ -1,5 +1,11 @@
 const db = require('./database.js');
 
+/**
+ * Get ID for a progress dot, creates one if it does not already exist
+ * @param {string} scriptId ID of the selected script
+ * @param {number} actorId ID of the selected actor
+ * @return {Promise<number>} ID of the newly created progress dot
+ */
 async function getId(scriptId, actorId) {
   const result = await db.query(
     `INSERT INTO read_position (actor_id, script_id, position)
@@ -13,19 +19,40 @@ async function getId(scriptId, actorId) {
   return result.rows[0].id;
 }
 
+/**
+ * Update the position of the progress dot
+ * @param {number} dotId ID of the current progress dot
+ * @param {string} position New position of the progress dot
+ */
 async function set(dotId, position) {
   await db.query(`UPDATE read_position SET position = $2 WHERE id = $1`, [dotId, position]);
 }
 
+/**
+ * Get all the progress dots for the selected script
+ * @param {string} scriptId ID of the selected script
+ * @return {Promise<Array.<Dot>>}
+ */
 async function getAll(scriptId) {
   const result = await db.query('SELECT id, actor_id, position FROM read_position WHERE script_id = $1', [scriptId]);
 
-  return result.rows.map((dot) => ({
-    id: dot.id,
-    actorId: dot.actor_id,
-    scriptId: scriptId,
-    position: dot.position,
-  }));
+  return result.rows.map((dot) => (new Dot(dot.id, dot.actor_id, scriptId, dot.position)));
+}
+
+/** Dot Class */
+class Dot {
+  /**
+   * @param {number} id ID of the progress dot
+   * @param {number} actorId ID of the actor associated with the dot
+   * @param {string} scriptId ID of the selected script
+   * @param {string} position Current position of the dot
+   */
+  constructor(id, actorId, scriptId, position) {
+    this.id = id;
+    this.actorId = actorId;
+    this.scriptId = scriptId;
+    this.position = position;
+  }
 }
 
 module.exports = { getId, set, getAll };

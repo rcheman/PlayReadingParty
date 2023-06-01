0daf9f7bd60df4a1f2fad7f2a42c810956e5a4c2
Matthew Cheman <mattcheman@pm.me>
2023-05-31 22:08:49 -0400

    Complete todo to cleanup postgres-db on exit of dev.sh

--- dev.sh
+++ dev.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
+
+# Pass "dev" as an argument to run the dev server
+
+# rebuild container
 podman image build -t play-reading . ;
 
-if [ ! "$(podman ps -a -q -f name=postgres-db)" ]; then
-    if [ "$(podman ps -aq -f status=exited -f name=postgres-db)" ]; then
-        # cleanup
-        podman rm postgres-db
-    fi
-    # run the container
-    podman run -v play_volume:/var/lib/postgresql/data \
-    --name postgres-db -e POSTGRES_PASSWORD=pass \
-    -e POSTGRES_USER=playreading -e POSTGRES_DB=playreadingparty \
-    -p 5432:5432 -d play-reading
+# run the container
+podman run -v play_volume:/var/lib/postgresql/data \
+--name postgres-db \
+-e POSTGRES_PASSWORD=pass \
+-e POSTGRES_USER=playreading \
+-e POSTGRES_DB=playreadingparty \
+-p 5432:5432 \
+-d play-reading
+
+function cleanup {
+  echo "Removing postgres-db container"
+  podman stop postgres-db
+  podman rm postgres-db
+}
+trap cleanup EXIT
+
+
+if [ "$1" = "dev" ]; then
+  # start development server
+  NODE_ENV=development \
+  concurrently \
+    "webpack-dev-server --color" \
+    "nodemon --inspect ./src/server.js"
+else
+  # start production server
+  node src/server.js
 fi
-
-# start development server
-NODE_ENV=development \
-concurrently "webpack-dev-server --color" "nodemon --inspect ./src/server.js"
-
-# TODO: add something that watches for when the server is exited, then kill and remove the container

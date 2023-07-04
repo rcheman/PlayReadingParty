2352d0cec6c90ea1a7895165637799363d7e5993
Rachel Cheman <rachelcheman@gmail.com>
2023-07-04 13:32:50 -0400

    Adds database migrations for when we first connect to the database, this removes the need for a Dockerfile and ensures that our test databases and actual database schemas stay in sync.

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM docker.io/postgres:15.2
-
-COPY ./db-setup.sql /docker-entrypoint-initdb.d/
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use dotenvy::dotenv;
-use play_reading_party::{configure_database, get_server};
+use play_reading_party::{configure_database, get_server, migrate_database};
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -9,5 +9,6 @@ async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port");
     let database_uri = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let db_pool = configure_database(database_uri);
-    get_server(listener, db_pool)?.await
+    let migrated_pool = migrate_database(db_pool);
+    get_server(listener, migrated_pool.await)?.await
 }

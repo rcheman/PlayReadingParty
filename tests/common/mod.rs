5f6e0928681603ac43611698a798c6418a8f2a1a
Rachel Cheman <rachelcheman@gmail.com>
2023-07-07 13:29:53 -0400

    Adds more thorough tests for /actors and reduces code duplication.

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,7 +3,7 @@ use sqlx::{Pool, Postgres};
 use std::net::TcpListener;
 
 // Create a test application on a random port using the given database pool
-pub async fn spawn_app(pool: &Pool<Postgres>, end_url: &str) -> TestApp {
+pub async fn setup_test_environment(pool: &Pool<Postgres>, end_url: &str) -> TestApp {
     let ip = "127.0.0.1";
     let listener = TcpListener::bind(format!("{}:0", ip))
         .expect("A random port should be able to be found to bind to");
